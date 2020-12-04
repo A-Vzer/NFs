c90bd58cafe2577d21b2ff170173a07668f83b03
A-Vzer <amaanmv@gmail.com>
2020-12-04 15:56:25 +0100

    major error fixed

--- datasets.py
+++ datasets.py
@@ -1,9 +1,12 @@
-from Datesets import Cifar
+from Datasets import Cifar, Svhn
 
 
 class Dataset:
     def __init__(self, nameDataset, dataroot, dataAugment, download, classNo):
         if nameDataset == 'cifar10':
             self.nameDataset = nameDataset
-        self.data = Cifar.Cifar10(dataAugment, dataroot, download, classNo)
+            self.data = Cifar.Cifar10(dataAugment, dataroot, download, classNo)
+        if nameDataset == 'svhn':
+            self.nameDataset = nameDataset
+            self.data = Svhn.Svhn(dataAugment, dataroot, download, classNo)
 
